76141be51ca68780af45bd7d3abd2c607fdd8090
Cøda <94078504+coda-damaged@users.noreply.github.com>
2024-02-06 16:32:54 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function setup() {
+	chatbot.loadFiles(['bot.rive']);
+}
+
+window.onload = setup;
+
+
+function theysaidno(){
+	setTimeout(kms, 3000);
+	return "" ;
+}
+
+//hope the children dont understand code
+function kms(){
+	location.reload();
+}
+
+
+function duck(){
+	setTimeout(racc, 3000);
+	return "";
+}
+
+function racc(){
+	window.close();
+}
+
